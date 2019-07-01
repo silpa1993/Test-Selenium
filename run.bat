69e4d0bacdfbb0d4d59a2a1dd703ee4c40a28551
silpa1993 <52377197+silpa1993@users.noreply.github.com>
2019-07-02 01:31:09 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
-set path=C:\Program Files\Java\jdk1.8.0_181\bin;C:\Windows\System32
 set classpath=C:\Users\SILPA\.jenkins\workspace\Test\lib\*;C:\Users\SILPA\.jenkins\workspace\Test\bin
 cd C:\Users\SILPA\.jenkins\workspace\Test
 java org.testng.TestNG testng.xml
