c8d05ac6cf3264b71a41d888c2286e0278c1657d
silpa <mssilpams93@gmail.com>
2019-07-02 01:23:20 +0530

    update run

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
-set projectLocation=C:\Users\SILPA\.jenkins\workspace\Test
+set path=C:\Program Files\Java\jdk1.8.0_181\bin;C:\Windows\System32
+set classpath=C:\Users\SILPA\.jenkins\workspace\Test\lib\*;C:\Users\SILPA\.jenkins\workspace\Test\bin
 cd C:\Users\SILPA\.jenkins\workspace\Test
-java -cp C:\Users\SILPA\.jenkins\workspace\Test\bin;C:\Users\SILPA\.jenkins\workspace\Test\lib* org.testng.TestNG testng.xml
+java org.testng.TestNG testng.xml
